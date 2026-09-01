822ee8b12e4811f8e773a483ff09e95e38a3ea2b
lataciagaskins-ship-it <292257288+lataciagaskins-ship-it@users.noreply.github.com>
2026-08-31 22:28:29 -0700

    Update header info

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
-// CSCI 27X - GitHub Upload Practice
-// Name: YOUR NAME
-// Section: YOUR SECTION
+// CSCI 271 - GitHub Upload Practice
+// Name: Latacia Gaskins
+// Section: 1290
 
 #include <iostream>
 using namespace std;
